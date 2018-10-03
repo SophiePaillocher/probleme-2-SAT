a92a6f57c00e08724e91ed2c5c5dbe511170cc5f
SophiePaillocher <sophie.paillocher@hotmail.fr>
2018-10-03 09:54:12 +0200

    composantes fortement connexes ok

--- src/com/company/Data.java
+++ src/com/company/Data.java
@@ -32,6 +32,7 @@ public class Data {
         }
         return graph;
     }
+    
 
 }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Data d = new Data();
         Graph g = d.implicationGraph();
-        System.out.println(g.toString());
+        System.out.println("Liste d'incidence du graphe des implications : " + "\n"+ g.toString());
+        System.out.println("Composantes fortement connexes : " + g.stronglyConnectedComponent());
     }
 }
